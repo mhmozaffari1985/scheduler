f5ac995e61b00686d9447cf0bdb303235e0db60d
mh.mozaffari <mh.mozaffari1985@gmail.com>
2021-08-07 19:45:01 -0400

    More Appointment Views

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,8 +2,6 @@ import React from "react";
 
 import "components/Appointment/styles.scss";
 
-import classnames from "classnames";
-
 export default function Appointment(props) { 
   return (
     <article className="appointment"></article>
