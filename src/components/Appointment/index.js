482776c5313d9b46bbe2339313951671d6b5d444
mh.mozaffari <mh.mozaffari1985@gmail.com>
2021-08-08 10:53:13 -0400

    Displaying Appointments

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,8 +2,28 @@ import React from "react";
 
 import "components/Appointment/styles.scss";
 
+import Header from "components/Appointment/Header";
+import Show from "components/Appointment/Show";
+import Empty from "components/Appointment/Empty";
+
 export default function Appointment(props) { 
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+      <Header
+          time={props.time}
+      />
+      {
+        props.interview ? 
+        <Show
+          student={props.student}
+          interviewer={props.interview.interviewer}
+          onEdit={props.onEdit}
+          onDelete={props.onDelete}
+        /> :
+        <Empty
+          onAdd={props.onAdd}
+        /> 
+      }
+    </article>
   );
  }
