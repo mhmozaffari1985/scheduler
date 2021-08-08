d9a98a72b9df832176f79f229e59e6cfbb355838
mh.mozaffari <mh.mozaffari1985@gmail.com>
2021-08-08 10:07:29 -0400

    Building the Appointment Form

---
+++ src/components/Appointment/Form.js
@@ -0,0 +1,39 @@
+import React , { useState } from "react";
+
+import "components/Appointment/styles.scss";
+
+import InterviewerList from "components/InterviewerList";
+import Button from "components/Button";
+
+export default function Form(props) { 
+  const [name, setName] = useState(props.name || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  function reset() {
+    setName("");
+    setInterviewer(null);
+    props.onCancel();
+  }
+  return (
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+        <form autoComplete="off">
+          <input
+            className="appointment__create-input text--semi-bold"
+            name="name"
+            type="text"
+            value={name}
+            placeholder="Enter Student Name"
+            onChange={(event) => setName(event.target.value)}
+          />
+        </form>        
+          <InterviewerList interviewers={props.interviewers} value={interviewer} onChange={setInterviewer} />       
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button danger onClick={reset}>Cancel</Button>
+          <Button confirm onClick={(e) => {props.onSave(name, interviewer)}}>Save</Button>
+        </section>
+      </section>
+    </main>
+  );
+ }
--- stories/index.js
+++ stories/index.js
@@ -18,6 +18,7 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 
 storiesOf("Button", module)
@@ -169,5 +170,21 @@ storiesOf("Appointment", module)
     <Error 
       message="Could not delete appointment."
       onClose={action("onClose")}
+    />
+  ))
+  .add("Form Edit", () => (
+    <Form 
+      name="Student Name"
+      interviewers={interviewers}
+      interviewer={3}
+      onSave={action("onSave")}
+      onCancel={action("onCancel")}
+    />
+  ))
+  .add("Form Create", () => (
+    <Form 
+      interviewers={interviewers}      
+      onSave={action("onSave")}
+      onCancel={action("onCancel")}
     />
   ));
