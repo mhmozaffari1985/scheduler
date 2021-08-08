c28ac12ffebe84b182dfd9af5b671bb21a5fc5d8
mh.mozaffari <mh.mozaffari1985@gmail.com>
2021-08-08 11:12:30 -0400

    The Schedule

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 
 import "components/Application.scss";
+
+import Appointment from "components/Appointment/index"
 import DayList from "components/DayList.js";
 
 const days = [
@@ -21,6 +23,62 @@ const days = [
   },
 ];
 
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Cohen",
+      interviewer: {
+        id: 2,
+        name: "Tori Malcolm",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "4pm",
+    interview: {
+      student: "Maria Boucher",
+      interviewer: {
+        id: 3,
+        name: "Mildard Nazir",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 6,
+    time: "5pm",
+  },
+];
+
+const appointmentList = appointments.map(appointment =>  {
+  return (<Appointment key={appointment.id} {...appointment} />);
+}
+)
+
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
   return (
@@ -36,7 +94,7 @@ export default function Application(props) {
           <DayList
             days={days}
             day={day}
-            setDay={setDay}
+            setDay={day => setDay(day)}
           />
         </nav>
         <img
@@ -47,7 +105,7 @@ export default function Application(props) {
         
       </section>
       <section className="schedule">
-        
+      {appointmentList}
       </section>
     </main>
   );
